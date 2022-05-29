30780aab0fdcaacd59bd4ccbfa45d9638ee5a780
peteykienzzz <pmatczak2@gmail.com>
2022-05-29 14:02:55 -0700

    building and decoding the translation matrix

--- route_cipher_decrypt.py
+++ route_cipher_decrypt.py
@@ -84,8 +84,35 @@ def key_to_int(key):
     key_int = [int(i) for i in key.split()]
     key_int_lo = min(key_int)
     key_int_hi = max(key_int)
-    if len(key_int) != COLS or key_int_lo < -COLS or key_int_hi > COLS or 0 in key_int_hi:
+    if len(key_int) != COLS or key_int_lo < -COLS or key_int_hi > COLS or 0 in key_int:
         print("\nError - Problem with key. Terminating.", file=sys.stderr)
         sys.exit(1)
     else:
         return key_int
+
+def build_matrix(key_int, cipherlist):
+    """Turn every n items in a list into a new item in a list of lists."""
+    translation_matrix = [None] * COLS
+    start = 0
+    stop = ROWS
+    for k in key_int:
+        if k < 0:  # read bottom-to-top of column
+            col_items = cipherlist[start:stop]
+        elif k > 0:  # read top-to-bottom of columnn
+            col_items = list((reversed(cipherlist[start:stop])))
+        translation_matrix[abs(k) - 1] = col_items
+        start += ROWS
+        stop += ROWS
+    return translation_matrix
+
+def decrypt(translation_matrix):
+    """Loop through nested lists popping off last item to a string."""
+    plaintext = ''
+    for i in range(ROWS):
+        for matrix_col in translation_matrix:
+            word = str(matrix_col.pop())
+            plaintext += word + ' '
+    return plaintext
+
+if __name__ == "__main__":
+    main()
