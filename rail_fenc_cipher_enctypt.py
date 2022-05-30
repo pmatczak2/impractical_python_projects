d6c3568b56c366a1415e4491ebe35a0b86200b3d
peteykienzzz <pmatczak2@gmail.com>
2022-05-30 10:32:50 -0700

    INSTRUCTING the user and getting input

---
+++ rail_fenc_cipher_enctypt.py
@@ -0,0 +1,20 @@
+r"""Encrypt a Civil 'rail fence' type cipher.
+THis is for a "2-rail" fence cipher for short messages.
+Example tect to encrypt:  'Buy more Maine potatose'
+Rail fence style: B Y O E A N P T T E
+                  U M T M I E O A O S
+
+Read zigzag:      \/\/\/\/\/\/\/\/\/\/\/
+
+Encrypted: BYOEA NPTTE UMRMI EOSOS
+
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------
+# USER INPUT:
+
+# the stringg to be encrypted (paste between quotes):
+plaintext = """Let us cross over the river and rest under the shade of the trees"""
+
+# END OF USER INPUT - DO NOT EDIT VELOW THIS LINE!
+#-----------------------------------------------------------------------------------------------------------------------
