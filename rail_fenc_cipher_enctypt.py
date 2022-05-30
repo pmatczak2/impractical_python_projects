3ccb6bcc1772d30c3b2cc9d02053fa96397f2a80
peteykienzzz <pmatczak2@gmail.com>
2022-05-30 11:08:37 -0700

    Encrypting a message

--- rail_fenc_cipher_enctypt.py
+++ rail_fenc_cipher_enctypt.py
@@ -1,6 +1,6 @@
 r"""Encrypt a Civil 'rail fence' type cipher.
 THis is for a "2-rail" fence cipher for short messages.
-Example tect to encrypt:  'Buy more Maine potatose'
+Example text to encrypt:  'Buy more Maine potatose'
 Rail fence style: B Y O E A N P T T E
                   U M T M I E O A O S
 
@@ -13,8 +13,38 @@ Encrypted: BYOEA NPTTE UMRMI EOSOS
 #-----------------------------------------------------------------------------------------------------------------------
 # USER INPUT:
 
-# the stringg to be encrypted (paste between quotes):
+# the string to be encrypted (paste between quotes):
 plaintext = """Let us cross over the river and rest under the shade of the trees"""
 
-# END OF USER INPUT - DO NOT EDIT VELOW THIS LINE!
+# END OF USER INPUT - DO NOT EDIT BELOW THIS LINE!
 #-----------------------------------------------------------------------------------------------------------------------
+
+def main():
+    """Run program to encrypt message using 2-rail rail fence cipher."""
+    message = prep_plaintext(plaintext)
+    rails = build_rails(message)
+    encrypt(rails)
+
+def prep_plaintext(plaintext):
+    """Remove spaces & leading/traliling whitespace."""
+    message = "".join(plaintext.split())
+    message = message.upper()  # convert for ciphertext is uppercase
+    print(f"\nplaintext = {plaintext}")
+    return message
+
+def build_rails(message):
+    """Build string with every other letter in a message."""
+    evens = message[::2]
+    odds = message[1::2]
+    rails = evens + odds
+    print(f"{rails}: {len(rails)}")
+    return rails
+
+def encrypt(rails):
+    """Split letters in ciphertext into chunks of 5 & join to make string."""
+    ciphertext = " ".join([rails[i:i+5] for i in range(0, len(rails), 5)])
+    print(f"ciphertext = {ciphertext}")
+    print(f"{len(ciphertext)}")
+
+if __name__ == "__main__":
+    main()
